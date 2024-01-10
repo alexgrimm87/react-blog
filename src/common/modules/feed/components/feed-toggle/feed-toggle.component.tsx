1481a96907c1746d3e78336befd6c9d815f7c1ca
Oleksandr Grymashevych <alexgrimm87@gmail.com>
2024-01-10 17:33:32 +0200

    add feed toggle

---
+++ src/common/modules/feed/components/feed-toggle/feed-toggle.component.tsx
@@ -0,0 +1,16 @@
+import {FC} from "react";
+import {NavLink} from "react-router-dom";
+
+interface FeedToggleProps {}
+
+export const FeedToggle: FC<FeedToggleProps> = () => {
+  return (
+    <div className="h-8">
+      <ul className="flex">
+        <li>
+          <NavLink to="/" className="bg-white border-b-2 border-blog-green py-2 px-4">Global Feed</NavLink>
+        </li>
+      </ul>
+    </div>
+  );
+};
--- src/common/modules/feed/components/feed/feed.component.tsx
+++ src/common/modules/feed/components/feed/feed.component.tsx
@@ -1,5 +1,6 @@
 import {FC} from "react";
 import {Container} from "../../../../components/container/container.component";
+import {FeedToggle} from "../feed-toggle/feed-toggle.component";
 import {ArticleList} from "../article-list/article-list.component";
 
 interface FeedProps {}
@@ -7,6 +8,7 @@ interface FeedProps {}
 export const Feed: FC<FeedProps> = () => {
   return (
     <Container>
+      <FeedToggle />
       <div className="flex">
         <ArticleList />
         <div className="w-1/4">tags</div>
